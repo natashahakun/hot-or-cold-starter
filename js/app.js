3aa00fdc583b5f8397a4233230e78c9930e3d8de
natashalynne <hakun.natasha@gmail.com>
2015-10-14 14:07:43 -0400

    Update functions.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -14,4 +14,25 @@ $(document).ready(function(){
 
 });
 
+var rand = Math.round(Math.random() * 101);
+var count = 0;
 
+var guessingGame = function() {
+  var guess = document.getElementById("userGuess").value;
+    if (guess < rand) {
+      document.getElementById("feedback").innerHTML = "Too low!";
+    } else if (guess > rand) {
+        document.getElementById("feedback").innerHTML = "Too high";
+    } else if (guess == rand) {
+        document.getElementById("feedback").innerHTML = "You got it! Game over!"
+    }
+  document.getElementById("guessList").innerHTML += guess + "  ";
+  document.getElementById("userGuess").value = "";
+  count += 1;
+  document.getElementById("count").innerHTML = count;
+}
+
+window.onload = function(){
+    var form = document.getElementById("guessButton");
+    form.addEventListener("click", guessingGame)
+}
