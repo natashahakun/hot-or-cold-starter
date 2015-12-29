2316c149cc234cefc40d7b659769d81660fc774d
natashalynne <hakun.natasha@gmail.com>
2015-12-28 22:29:18 -0500

    Clean-up css and js.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 $(document).ready(function(){
 
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  	$("#what").click(function(){
+    	$(".overlay").fadeIn(500);
 
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
+  		$(".overlay").fadeOut(500);
   	});
 
 });
@@ -20,24 +20,28 @@ var count = 0;
 var guessingGame = function() {
   var guess = document.getElementById("userGuess").value;
   if (isNaN(guess) === true || guess < 1 || guess > 100) {
-    document.getElementById("feedback").innerHTML = "Please enter a number between 1 and 100."
-    } else if (guess <= (rand - 50) || guess >= (rand + 50)) {
-        document.getElementById("feedback").innerHTML = "Ice cold!";
-    } else if ((guess >= (rand - 49) && guess <= (rand - 30)) || (guess <= (rand + 49) && guess >= (rand + 30))) {
-        document.getElementById("feedback").innerHTML = "Cold";
-     } else if ((guess >= (rand - 29) && guess <= (rand - 20)) || (guess <= (rand + 29) && guess >= (rand + 20))) {
-        document.getElementById("feedback").innerHTML = "Warm";
-     } else if ((guess >= (rand - 19) && guess <= (rand - 10)) || (guess <= (rand + 19) && guess >= (rand + 10))) {
-        document.getElementById("feedback").innerHTML = "Hot!";
-     } else if ((guess >= (rand - 9) && guess <= (rand - 1)) || (guess <= (rand + 9) && guess >= (rand + 1))) {
-        document.getElementById("feedback").innerHTML = "Very hot!";
-    } else if (guess == rand) {
-        document.getElementById("feedback").innerHTML = "You got it! Game over!"
-    }
-  document.getElementById("guessList").innerHTML += guess + " ";
+      document.getElementById("feedback").innerHTML = "Please enter a number between 1 and 100."
+  } else if (guess <= (rand - 50) || guess >= (rand + 50)) {
+      document.getElementById("feedback").innerHTML = "Ice cold!";
+  } else if ((guess >= (rand - 49) && guess <= (rand - 30)) || (guess <= (rand + 49) && guess >= (rand + 30))) {
+      document.getElementById("feedback").innerHTML = "Cold";
+  } else if ((guess >= (rand - 29) && guess <= (rand - 20)) || (guess <= (rand + 29) && guess >= (rand + 20))) {
+      document.getElementById("feedback").innerHTML = "Warm";
+  } else if ((guess >= (rand - 19) && guess <= (rand - 10)) || (guess <= (rand + 19) && guess >= (rand + 10))) {
+      document.getElementById("feedback").innerHTML = "Hot!";
+  } else if ((guess >= (rand - 9) && guess <= (rand - 1)) || (guess <= (rand + 9) && guess >= (rand + 1))) {
+      document.getElementById("feedback").innerHTML = "Very hot!";
+  } else if (guess == rand) {
+      document.getElementById("feedback").innerHTML = "You got it! Game over!"
+  }
+  
+  if (isNaN(guess) === false && guess >= 1 && guess <= 100) {
+    document.getElementById("guessList").innerHTML += guess + " ";
+    count += 1;
+    document.getElementById("count").innerHTML = count;
+  }
   document.getElementById("userGuess").value = "";
-  count += 1;
-  document.getElementById("count").innerHTML = count;
+
 }
 
 window.onload = function(){
